9c7529edb0df2a987e6be81c2f415319db8b427e
Bianca <biancadurgante.aluno@unipampa.edu.br>
2024-07-17 00:49:42 -0300

    carrega arquivo, printa acervo e busca por codigo

--- acervo.c
+++ acervo.c
@@ -3,23 +3,21 @@
 #include <string.h>
 #include "acervo.h"
 
-struct desc_LSE * nova_lista(){  //cria e malloca o descritor
-        printf("Entrou na funcao malloca descritor");
-
+struct desc_LSE * nova_lista() {  // cria e malloca o descritor
     struct desc_LSE *novalista = (struct desc_LSE *)malloc(sizeof(struct desc_LSE));
-      if (novalista == NULL) {
-        printf("Erro ao alocar memória para o descritor\n");
+    if (novalista == NULL) {
+        printf("\nErro ao alocar memória para o descritor\n");
         exit(1);
     }
     novalista->LSE = NULL;
     novalista->tamanho = 0;
-    printf("Descritor criado com sucesso\n");
     return novalista;
 }
+
 struct musica * criaMusica(char * artista, int codigo, char * titulo, char * letra) {  // cria e malloca o meu dado, a musica
     struct musica *novamusica = (struct musica *)malloc(sizeof(struct musica));
     if (novamusica == NULL) {
-        printf("Erro ao alocar memória para a música\n");
+        printf("\nErro ao alocar memória para a música\n");
         exit(1);
     }
     strcpy(novamusica->titulo, titulo);
@@ -29,91 +27,78 @@ struct musica * criaMusica(char * artista, int codigo, char * titulo, char * let
     return novamusica;
 }
 
-
-struct nodo_LSE * cria_no(struct musica *novamusica){    //recebe o meu dado, no caso a minha musica e cria um nó
+struct nodo_LSE * cria_no(struct musica *novamusica) {  // recebe o meu dado, no caso a minha musica e cria um nó
     struct nodo_LSE *novoElemento = (struct nodo_LSE *)malloc(sizeof(struct nodo_LSE));
-	    printf("Entrou na insere cria no");
-
-    novoElemento->info=novamusica;
-	novoElemento->prox =  NULL;
-	return novoElemento;
-}
-
-int lista_vazia(struct desc_LSE *nova_lista){
-    int verificador=0;
-    if (nova_lista->tamanho==0){
-        verificador=1;
-        return verificador; //precisa retornar aqui pra ter o valor do teste
+    if (novoElemento == NULL) {
+        printf("\nErro ao alocar memória para o nó\n");
+        exit(1);
     }
-    return verificador;
+    novoElemento->info = novamusica;
+    novoElemento->prox = NULL;
+    return novoElemento;
 }
 
-struct desc_LSE * insere_lista(struct desc_LSE *nova_lista, struct musica * musica, int posicao){
-    int i;
+int lista_vazia(struct desc_LSE *nova_lista) {
+    return nova_lista->tamanho == 0;
+}
+
+struct desc_LSE * insere_lista(struct desc_LSE *nova_lista, struct musica * musica, int posicao) {
     struct nodo_LSE *no = cria_no(musica);
     struct nodo_LSE *aux = nova_lista->LSE;
-    int teste=0;
-    teste=lista_vazia(nova_lista);
+    int i = 0;
 
-    printf("Entrou na insere antes de inserir");
-    if(teste==1){  //insere na lista vazia no começo, nao importa posicao
-        nova_lista->LSE=no;
+    if (lista_vazia(nova_lista)) {  // insere na lista vazia no começo, nao importa posicao
+        nova_lista->LSE = no;
         nova_lista->tamanho++;
-        printf("Entrou na insere lista vazia");
-
         return nova_lista;
-    }
-    else{ //inserir no comeco lista nao vazia, posicao 0
-        if(posicao==0){
-			no->prox=aux;
-			nova_lista->LSE=no;
-			nova_lista->tamanho++;
-            printf("Entrou na insere posicao 0");
-			return nova_lista;
-    }
+    } else {  // inserir no começo lista nao vazia, posicao 0
+        if (posicao == 0) {
+            no->prox = aux;
+            nova_lista->LSE = no;
+            nova_lista->tamanho++;
+            return nova_lista;
+        }
 
-    
-        //inserir no meio
-		if(posicao >0 && posicao < nova_lista->tamanho){
-				while(i<posicao){
-					aux=aux->prox;
-					i++;
-				}
-				no->prox=aux->prox;
-                aux->prox=no;
-				nova_lista->tamanho++;
-				return nova_lista;
-		}
+        // inserir no meio
+        if (posicao > 0 && posicao < nova_lista->tamanho) {
+            while (i < posicao - 1) {
+                aux = aux->prox;
+                i++;
+            }
+            no->prox = aux->prox;
+            aux->prox = no;
+            nova_lista->tamanho++;
+            return nova_lista;
+        }
 
-        //inserir no fim
-		if(posicao>0 && posicao==nova_lista->tamanho || posicao>0 && posicao>nova_lista->tamanho){
-				while(aux->prox != NULL){
-					aux=aux->prox;
-				}
-				aux->prox=no;
-				nova_lista->tamanho++;
-                return nova_lista;
-				
-	}
-}
+        // inserir no fim
+        if (posicao >= nova_lista->tamanho) {
+            while (aux->prox != NULL) {
+                aux = aux->prox;
+            }
+            aux->prox = no;
+            nova_lista->tamanho++;
+            return nova_lista;
+        }
+    }
+    return nova_lista;
 }
 
-void imprimelista(struct desc_LSE *nova_lista){
-        struct nodo_LSE *aux=nova_lista->LSE;
+void imprimelista(struct desc_LSE *nova_lista) {
+    struct nodo_LSE *aux = nova_lista->LSE;
 
-    while(aux != NULL){
+    while (aux != NULL) {
         printf("\n------------------------------------------------");
         printf("\nNome: %s", aux->info->titulo);
         printf("Artista: %s", aux->info->artista);
         printf("Trecho: %s", aux->info->letra);
         printf("Codigo: %d\n", aux->info->codigo);
         printf("\n------------------------------------------------\n");
-    
-        aux=aux->prox;
+        aux = aux->prox;
     }
 }
 
 void limpar_buffer() {
-  int c;
-  while ((c = getchar()) != '\n' && c != EOF) { }
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) { }
 }
--- mainAcervo.c
+++ mainAcervo.c
@@ -3,41 +3,42 @@
 #include <string.h>
 #include "acervo.h"
 
+void quebraFrase(char *frase, int i, struct desc_LSE *acervo, struct musica *novaMusica);
 
-void quebraFrase(char *frase, struct desc_LSE * acervo, struct musica * novaMusica);
+int main() {
+    int i = 0;
+    char linha, frase[256];
+    struct musica *novaMusica = NULL;
+    struct desc_LSE *acervo = NULL;
 
+    acervo = nova_lista();  // cria e malloca o descritor
 
-int main(){
+    int menu, menubusca;
+    int acervospoty;
 
-int i=0, tamanhoacervo; //guarda o tamanho de linhas do arquivo igual o mercado
-char linha, frase[256]; //cada linha lida do arquivo até o barra n cai aqui e cada frase recebe a linha pra poder quebrar
-
-struct musica * novaMusica = NULL;
-struct desc_LSE * acervo = NULL;
-struct nodo_LSE * elemento = NULL;
-printf("Erro no arquivo.\n");
-acervo = nova_lista();  //cria e malloca o descritor
- if (acervo == NULL) {
-        printf("Erro ao criar lista.\n");
-        return 1;
-    }
-int menu;
- // leitura do arquivo texto
+    // leitura do arquivo texto
     FILE *arquivo = fopen("musicas.txt", "r");
     if (arquivo == NULL) {
         printf("Erro no arquivo.\n");
         return 1;
     }
 
+    //aqui eu pego a primeira linha para ler certo as musicas
+	fscanf(arquivo,"%d",&acervospoty); 
+	printf("O tamanho do seu acervo de musicas e: %d\n", acervospoty);
+    
     // leu o arquivo
     while ((linha = fgetc(arquivo)) != EOF) {
         if (linha != '\n') { // caractere diferente de \n guarda em frase
-            frase[i] = linha;
-            i++;
+            if (i < sizeof(frase) - 1) {
+                frase[i] = linha;
+                i++;
+            } 
+
         } else {
             if (i > 0) {
                 frase[i] = '\0';
-                quebraFrase(frase, acervo, novaMusica);
+                quebraFrase(frase, i, acervo, novaMusica);
                 i = 0; // reset para próxima linha
             }
         }
@@ -45,57 +46,80 @@ int menu;
 
     if (i > 0) {
         frase[i] = '\0';
-        quebraFrase(frase, acervo, novaMusica);
+        quebraFrase(frase, i, acervo, novaMusica);
     }
 
     fclose(arquivo);
-        
 
-        printf("\n------------------------------SEJA BEM VINDO AO SEU SPOTY------------------------------\n");
-        printf("Escolha uma das opcoes: \n");
-        printf("(1) Para mostrar o acervo\n(0) Para sair\n");
-        scanf("%d", &menu);
-        do{
-            switch(menu){
+    do {
+    printf("\n------------------------------SEJA BEM VINDO AO SEU SPOTY------------------------------\n");
+    printf("Escolha uma das opcoes: \n");
+    printf("(1) Mostrar o acervo\n(2) Busca\n(3) Criar playlist\n(0) Para sair\n");
+    scanf("%d", &menu);
+
+        switch (menu) {
+            case 1:
+                printf("\nO tamanho do seu acervo eh: %d", acervo->tamanho);
+                imprimelista(acervo);
+                break;
+            
+            case 2:
+                printf("\n(1) Busca por Codigo\n(2) Busca por Titulo\n(3) Busca por Artista\n");
+                scanf("%d", &menubusca);
                 
-                case 1:
+                switch(menubusca){
+                    case 1:
+                    int buscacodigo;
+                    printf("Digite o codigo da musica: \n");
+                    scanf ("%d", &buscacodigo);
+            
+                    struct nodo_LSE *aux = acervo->LSE;
 
-                    imprimelista(acervo);
+                    while (aux != NULL) {
+                        if(aux->info->codigo==buscacodigo){
+                            printf("\n------------------------------------------------");
+                            printf("\nNome: %s", aux->info->titulo);
+                            printf("Artista: %s", aux->info->artista);
+                            printf("Trecho: %s", aux->info->letra);
+                            printf("Codigo: %d\n", aux->info->codigo);
+                            printf("\n------------------------------------------------\n");
+                        }
+                        aux = aux->prox;
+                    }
+
+                    break;
+
+                    case 2:
+                    break;
+
+                    case 3:
+                    break;
+
+                    default:
+                    break;
+
+                }
+                
+                break; //break do menu principal
+                
+            default:
                 break;
+        }
+    } while (menu != 0);
 
-                default:
-                break;
-            }
-
-
-
-        } while (menu!=0);
-     return 0;
+    return 0;
 }
 
 
+// vai gerar o meu dado pra preencher o nó pra colocar na lista que vai ser o meu acervo
+void quebraFrase(char *frase, int i, struct desc_LSE * acervo, struct musica * novaMusica) {
+    char titulo[256], artista[256], letra[256];
 
-//vai gerar o meu dado pra preencher o nó pra colocar na lista que vai ser o meu acervo
+    strcpy(artista, strtok(frase, ";"));
+    int codigo = atoi(strtok(NULL, ";"));
+    strcpy(titulo, strtok(NULL, ";"));
+    strcpy(letra, strtok(NULL, ";"));
+    novaMusica = criaMusica(artista, codigo, titulo, letra);
 
-void quebraFrase(char *frase, struct desc_LSE * acervo, struct musica * novaMusica){
-	char titulo [256], artista [256], letra[256];
-	int posicao=0;
-	//nodo * novoNodo = NULL;
-
-	//printf("\n\n===================================\n");
-	//printf("artista é: %s \n",strtok(frase,";"));
-	strcpy(artista,strtok(frase,";"));
-	//printf("codigo é: %s \n",strtok(NULL,";"));
-	int codigo = atoi(strtok(NULL,";"));
-	//printf("titulo é: %s \n",strtok(NULL,";"));
-	strcpy(titulo,strtok(NULL,";"));
-	//printf("letra é: %s \n",strtok(NULL,";")); 
-	strcpy(letra,strtok(NULL,";"));
-
-	novaMusica = criaMusica(artista, codigo, titulo, letra);
-	//printf("===================================\n");
-	
-	insere_lista(acervo, novaMusica, posicao);
-
-	return;
+    insere_lista(acervo, novaMusica, 0);
 }
